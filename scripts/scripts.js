664439351636aca90ba5b0baed127728c2b4cf58
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2023-09-25 18:16:42 -0500

    Complete Refactor
    Completely refactored the entire game. This version is more optimized, modular and concise.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,194 +1,187 @@
 const gameDefaults = {
-	cell:[{
-		bomb: false,
+  columns: 30,
+	rows: 30,
+	started: false,
+	difficultySetting: 1,
+	difficulty: [50,80,140],
+	clearedCClass: "cleared",
+	score: 0,
+  grid: [],
+  bombLocations: [],
+  cellDefaults: {
+    bomb: false,
 		cleared: false,
-		nearby:0,
-		flagged:false,
-		leftEdge:false,
-		rightEdge:false,
-		topEdge:false,
-		bottomEdge:false
-
-	}],
-	gridWidth:30,
-	gridSize: 30*30,
-	started:false,
-	difficultySetting:1,
-	difficulty:[50,80,140],
-	clearedColor:"white",
-	unclearedColor:"grey",
-	flagged:"none",
-	score:0,
+		nearby: 0,
+    element: undefined,
+  },
+  directions: [[-1, 0], [-1, -1], [0, -1], [+1, -1], [+1, 0], [+1, +1], [0, +1], [-1, +1]] // For directional checks
 }
-let game = structuredClone(gameDefaults); // Clones the default values from the gameDefaults object into a new usable object called game.
+let game = structuredClone(gameDefaults);
 
+// Grid generation
+window.onload = function() {
+  for(y = 0; y < game.rows; y++) {
+    for(x = 0; x < game.columns; x++) {
+      if(y === 0) {
+        game.grid.push([]); // Initializes the columns on first passthrough.
+      }
+      const container = document.getElementById("grid"); // Stores the container element in variable called container
+      const element = document.createElement("div"); // stores the type of element (div) we want to append in variable called element
+      container.appendChild(element); // adds this element to the container
+      element.setAttribute('data-x', x);
+      element.setAttribute('data-y', y);
+      element.setAttribute('onClick', `cellClicked(${x}, ${y})`);
+      element.classList.add('cell');
+      game.grid[x][y] = structuredClone(game.cellDefaults); // Pushes a cell object with default values into [x][y] of game.grid.
+      game.grid[x][y].element = element; // Stores a reference to the dom element as a parameter called element.
+      element.addEventListener("contextmenu", function(event) {
+        rightClickFlagging(event, element); // Pass the event parameter to the function
+      });
+    }
+  }
+  gameStart(); // When the window loads, we call the gameStart() function with game.started as false and difficulty set the medium (defaults)
+}
 
+function rightClickFlagging(event, element) {
+  event.preventDefault(); // Prevent the default context menu
+  const flag = element.innerHTML;
 
-// Generate Grid
-$(document).ready(function(){
-  // Grid generation and cell setup
-	for(let i=0; i<game.gridSize; i++) {
-  		$("#grid").append("<div class='cell' onclick='checkCells("+i+")' data-pos='"+i+"'></div>");
-	}
-	
-	// Right-click functionality
-	$(".cell").contextmenu(function() {
-		let clickLoc = $(this).data('pos');
-	  let $cell = $(".cell").eq(clickLoc);
-	  if (game.cell[clickLoc].cleared === false) {
-	    switch (game.cell[clickLoc].flagged) {
-	      case "!":
-	        $cell.html("?");
-	        game.cell[clickLoc].flagged = "?";
-	        break;
-	      case "?":
-	        $cell.html("");
-	        game.cell[clickLoc].flagged = "";
-	        break;
-	      default:
-	        $cell.html("!");
-	        game.cell[clickLoc].flagged = "!";
-	    }
-	  }
-	  return false; // Prevents right click popup menu.
-});
-	gameStart();
-})
+  switch(flag) {
+
+    case "":
+      element.innerHTML = "?";
+      break;
+    
+    case "?":
+      element.innerHTML = "!";
+      break;
+
+    case "!":
+      element.innerHTML = "";
+      break;
+  }
+}
+
+function difficultyClick(index, difficulty) {
+  if(confirm(`Do you want to start a new game with difficulty: ${difficulty}?`)) {
+    game.started = true;
+    element = document.getElementById("diff");
+    element.innerHTML = difficulty;
+    game.difficultySetting = parseInt(index);
+    gameStart();
+  }
+}
 
 function gameStart() {
-	game.started = true;
-	for(i=0; i<game.gridSize; i++) {
-			game.cell[i] = structuredClone(gameDefaults.cell[0]); // Populates the cells with default values
-  		if(i % game.gridWidth  === 0) { // Checks if current cell is on left edge.
-  			game.cell[i].leftEdge = true;
-  		}
-  		if(i%game.gridWidth+1 === game.gridWidth) { // Checks if current cell is on the right edge
-  			game.cell[i].rightEdge = true;
-  		}
-  		if(i<game.gridWidth) { // Checks if the current cell is on the top row/edge.
-  			game.cell[i].topEdge = true;
-  		}
-  		if(i>=game.gridSize-game.gridWidth) { // Checks if the current cell is on the bottom row/edge.
-  			game.cell[i].bottomEdge = true;
-  		}
-  		// recolor bg logic
-  		$(".cell").eq(i).css("background-color","grey");
-  		// get rid of added html logic
-  		$(".cell").eq(i).html("");
-  	}
-  	generateBombs(game.difficulty[game.difficultySetting]);
+  console.log("Game started called");
+  game.started ? resetGame() : game.started = true; // If the game already started when this is called, we reset the game before starting the game again.
+  const diffIndex = game.difficultySetting;
+  const maxBombs = game.difficulty[diffIndex];
+  generateBombs(maxBombs);
 }
 
-$("button").click(function(){
-	if(confirm("Do you want to reset the game on "+$(this).data('diff').toLowerCase()+" difficulty?")) {
-		game.difficultySetting = parseInt($(this).data("speed"));
-		gameStart();
-		$("#diff").html($(this).data("diff"));
-	}
-});
-
-// Cell clearing and checking logic
-function checkCells(clickedCell) { // Parameter takes the position value from the clicked cell.
-	let cellIndex = [clickedCell]; // Adds the clicked cell's position to the array that holds the cell positions to check.
-	for(let i=0; i<cellIndex.length; i++) { // Iterates through the array of cell positions we need to check
-		let x = cellIndex[i]; // sets the value of x to the position at array index i -- basically, x gets the position on the grid (from the array) we need to check during each iteration
-		if(!game.cell[x].cleared && game.started && !game.cell[x].bomb) { // Checks if the currenct cell isn't cleared, has no bombs and the game hasn't ended
-			clearThisCell(cellIndex[i]); 
-			if(game.cell[x].nearby === 0) {
-				// directional checks here -- check direction AND whether the adjacent cell is already in cellIndex[]
-				if(!game.cell[x].leftEdge && !cellIndex.includes(x-1)) { // Check current cell for being on the left
-					cellIndex.push(x-1); // pushes the cell position to the cellIndex array so that it is evalulated/checked during the loop
-				}
-				if(!game.cell[x].rightEdge && !cellIndex.includes(x+1)) { // Check if current cell isn't on the right edge.
-					cellIndex.push(x+1); 
-				}
-				if(!game.cell[x].topEdge && !cellIndex.includes(x-game.gridWidth)) { // Checks if current cell is not on the top row/edge.
-					cellIndex.push(x-game.gridWidth); 
-				}
-				if(!game.cell[x].bottomEdge && !cellIndex.includes(x+game.gridWidth)) { // Checks if the current cell is on the bottom row/edge.
-					cellIndex.push(x+game.gridWidth);
-				}
-				if(!game.cell[x].topEdge && !game.cell[x].leftEdge && !cellIndex.includes(x-(game.gridWidth+1))) { // Checks if the current cell is either on the left or top edges.
-					cellIndex.push(x-(game.gridWidth+1));
-				}
-				if(!game.cell[x].topEdge && !game.cell[x].rightEdge && !cellIndex.includes(x-(game.gridWidth-1))) { // Checks if the current cell is either on the right or the top edges.
-					cellIndex.push(x-(game.gridWidth-1));
-				}
-				if(!game.cell[x].bottomEdge && !game.cell[x].rightEdge && !cellIndex.includes(x+game.gridWidth+1)) { // Checks if the current cell is either on the right or the bottom edges.
-					cellIndex.push(x+game.gridWidth+1);
-				}
-				if(!game.cell[x].leftEdge && !game.cell[x].bottomEdge && !cellIndex.includes(x+(game.gridWidth-1))) { // Checks if the current cell is either on the left or bottom edges.
-					cellIndex.push(x+(game.gridWidth-1));
-				}
-			}
-		} else if(game.cell[x].bomb === true && game.started === true) {
-			game.started = false;
-			for(let b=0; b<game.gridSize; b++) {
-				if(game.cell[b].bomb === true) {
-					$(".cell").eq(b).css("background-color","red");
-				}
-			}
-			alert("You clicked a bomb! You lose! Press ok and choose a difficulty to play again.");
-		}
-	}
+// Generates bombs based on difficulty setting.
+function generateBombs(maxBombs) {
+  console.log("Generate Bombs called");
+  for(i = 0; i < maxBombs; i++) {
+    let bombGenerated = false;
+    while(!bombGenerated) {
+      const ranX = Math.floor(Math.random()*(((game.columns-1)+1)-0)+0); // Generates a random x coordinate.
+      const ranY = Math.floor(Math.random()*(((game.rows-1)+1)-0)+0); // Generates a random y coordinate.
+      if(!game.grid[ranX][ranY].bomb) { // If this location does not have a bomb, we set one here.
+        game.grid[ranX][ranY].bomb = true;
+        updateNearby(ranX, ranY);
+        game.bombLocations.push([ranX, ranY]); // Add the coordinates to the bombLocations array.
+        bombGenerated = true;
+      }
+    }
+  }
 }
 
-// Function to clear the current cell only.
-function clearThisCell(i) {
-	game.cell[i].cleared = true;
-	game.cell[i].flagged = "none";
-	$(".cell").eq(i).css("background-color",game.clearedColor); // change this later for better visuals
-	$(".cell").eq(i).html(""); // Removes any flags
-	if(game.cell[i].nearby !== 0) {
-		$(".cell").eq(i).html("<span style='color:blue'>"+game.cell[i].nearby+"</span>"); // logic for displaying the nearby number on the cell here
-	}
-	let element = document.getElementById("scount");
-	element.innerHTML = `${game.score += 10}`;
+function updateNearby(x, y) {
+  for(const adjustment of game.directions) { // Cycle through each coordinate adjustments found in the directions array
+    const dx = adjustment[0]; // Store the x coordinate adjustment
+    const dy = adjustment[1]; // Store the y coordinate adjustment
+    if(isInsideGrid(x+dx, y+dy) && !game.grid[x+dx][y+dy].bomb) { // If this new cell 
+      game.grid[x+dx][y+dy].nearby++; // Increment its nearby property to show how many bombs are nearby.
+    }
+  }
 }
 
-function generateBombs(amount) {
-	for(i=0; i<amount; i++) {
-		let bombGenerated = false;
-		while(!bombGenerated) {
-			let a = randomNumber(0,game.gridSize-1);
-			if(game.cell[a].bomb === false) {
-				game.cell[a].bomb = true;
-				bombGenerated = true;
-				updateNearby(a);
-			}
-		}
-	}
+function cellClicked(a, b) {
+  console.log(`Clicked cell X: ${a}, Y: ${b}`);
+  console.log(game.grid[a][b]);
+  if(!game.started) { // If the game hasn't started, clicking will do nothing.
+    return;
+  }
+  a = parseInt(a); // To avoid any wonky string business
+  b = parseInt(b); // To avoid any wonky string business
+  const clearTheseCells = []; // Create an array that will hold all cells we need to clear.
+  if(!game.grid[a][b].cleared && !game.grid[a][b].bomb) { // If the cell we just clicked is cleared, does not contain a bomb and the game has started
+    clearTheseCells.push([a, b]); // We push it to the array of cells we need to clear
+    for(i = 0; i < clearTheseCells.length; i++) {
+      const x = clearTheseCells[i][0];
+      const y = clearTheseCells[i][1];
+      clearCell(x, y); // Reveals the cell
+      if(game.grid[x][y].nearby === 0) { // If the cell just cleared has 0 nearby, we clear all cells adjacent to them by pushing them into the clearTheseCells array.
+        for(const adjustment of game.directions) { // Cycles through each set of coordinates from the directions array
+          const newX = x+adjustment[0];
+          const newY = y+adjustment[1];
+          if(isInsideGrid(newX, newY) && !alreadyInClearArray(newX, newY)) { // If this new cell location is inside of the grid area and not already in the clearTheseCells array,
+            clearTheseCells.push([newX, newY]); // we add it to the clearTheseCells array.
+          }
+        }
+      }
+    }
+  } else if(game.grid[a][b].bomb) {
+    game.started = false;
+    revealBombs();
+    alert("You clicked a bomb. GAME OVER! Choose a difficulty setting to start a new game.")
+  }
+  // Helper function to check whether a cell's coordinates were already added to the clearTheseCells array.
+  function alreadyInClearArray(x, y) {
+    for(const coordinate of clearTheseCells) { // Cycle through the coordinates in clearTheseCells
+      if(coordinate[0] === x && coordinate[1] === y) { // If both the x and y coordinates match, then the cell coordinates already exist in the array and we return true
+        return true;
+      }
+    }
+    return false; // Returns false if no matches were found during the for of loop
+  }
 }
 
-// Generate a random number between two values, inclusive. Still need to go around and update the places I need to use this.
-function randomNumber(min, max) {
-  return Math.floor(Math.random()*((max+1)-min)+min);
+function revealBombs() {
+  for(const coordinates of game.bombLocations) {
+    const x = coordinates[0];
+    const y = coordinates[1];
+    game.grid[x][y].element.classList.add("bombreveal");
+  }
 }
 
-// function for updating nearby cells ".nearby" values once a bomb is placed
-function updateNearby(i) {
-	if(!game.cell[i].leftEdge) { // Check current cell for being on the left
-		game.cell[i-1].nearby++;
-	}
-	if(!game.cell[i].rightEdge) { // Check if current cell isn't on the right edge.
-		game.cell[i+1].nearby++; 
-	}
-	if(!game.cell[i].topEdge) { // Checks if current cell is not on the top row/edge.
-		game.cell[i-game.gridWidth].nearby++; 
-	}
-	if(!game.cell[i].bottomEdge) { // Checks if the current cell is on the bottom row/edge.
-		game.cell[i+game.gridWidth].nearby++;
-	}
-	if(!game.cell[i].topEdge && !game.cell[i].leftEdge) { // Checks if the current cell is either on the left or top edges.
-		game.cell[i-(game.gridWidth+1)].nearby++;
-	}
-	if(!game.cell[i].topEdge && !game.cell[i].rightEdge) { // Checks if the current cell is either on the right or the top edges.
-		game.cell[i-(game.gridWidth-1)].nearby++;
-	}
-	if(!game.cell[i].bottomEdge && !game.cell[i].rightEdge) { // Checks if the current cell is either on the right or the bottom edges.
-		game.cell[i+game.gridWidth+1].nearby++;
-	}
-	if(!game.cell[i].leftEdge && !game.cell[i].bottomEdge) { // Checks if the current cell is either on the left or bottom edges.
-		game.cell[i+(game.gridWidth-1)].nearby++;
-	}
+// Helper function to make checking whether a coordinate is within the grid.
+function isInsideGrid(x, y) {
+  return x >= 0 && x < game.columns && y >= 0 && y < game.rows;
+}
+
+function clearCell(x, y) {
+  cell = game.grid[x][y];
+  cell.cleared = true;
+  cell.flagged = "";
+  cell.element.classList.add("cleared");
+  cell.nearby !== 0 ? cell.element.innerHTML = cell.nearby : cell.element.innerHTML = "";
+}
+
+function resetGame() {
+  console.log("Reset game called");
+  game.bombLocations = [];
+  // Iterate through every cell and reset to default values (preserve element)
+  for(y = 0; y < game.rows; y++) {
+    for(x = 0; x < game.columns; x++) {
+      const element = game.grid[x][y].element; // Store a temporay reference to the cell's respective DIV element.
+      game.grid[x][y] = structuredClone(game.cellDefaults) // Restore defaults
+      game.grid[x][y].element = element; // Restore the DIV reference
+      game.grid[x][y].element.classList.remove("cleared"); // Remove cleared styling
+      game.grid[x][y].element.classList.remove("bombreveal"); // Remove bombreveal styling
+      game.grid[x][y].element.innerHTML = ""; // Remove any innerHTML
+    }
+  }
 }
